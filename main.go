fd03ce894d6dd1f01aede5bf88d155751ee57188
amasok <amasok32@gmail.com>
2021-12-12 12:07:46 +0900

    zapでlog出力する際にオブジェクト内にあるマスクしたい項目(個人情報など)をマスクする方法

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const MASKED_STR = "********"
+
+type user struct {
+	ID        string
+	Name      string
+	Email     string
+	CreatedAt time.Time
+}
+
+// zapを使った構造体のマスク方法
+func (u user) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("id", u.ID)
+	enc.AddString("name", MASKED_STR)
+	enc.AddString("email", MASKED_STR)
+	enc.AddInt64("created_at", u.CreatedAt.UnixNano())
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello world")
+	logger, _ := zap.NewDevelopment()
+	user := &user{
+		ID:        "9d3d893d9d3daofa",
+		Name:      "Zap",
+		Email:     "zap@sample.com",
+		CreatedAt: time.Now(),
+	}
+	logger.Info("object log sample", zap.Object("user", user))
 }
